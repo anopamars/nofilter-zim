ed8b9311dee4b444768834a0a353d30190ec2a5f
anopamars <anopamaramba@gmail.com>
2026-05-02 16:19:04 +0200

    fix scrolling on all onboarding pages, consent screen, terms and privacy

--- app.js
+++ app.js
@@ -13,4 +13,9 @@ function clearData() {
   localStorage.removeItem('nfz_profile');
   localStorage.removeItem('nfz_answers');
   localStorage.removeItem('nfz_response_count');
+  localStorage.removeItem('nfz_expires_at');
+  localStorage.removeItem('nfz_views');
+  localStorage.removeItem('nfz_roast_score');
+  localStorage.removeItem('nfz_leaderboard');
+  localStorage.removeItem('nfz_current_battle');
 }
